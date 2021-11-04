1d77377091e010cea5559a06cd7a3a13aa060fff
VanCauwenbergIan <ian.van.cauwenberg@student.howest.be>
2021-11-04 22:03:17 +0100

    Second class added to handle JSON
    Class only gets the results array and saves the name and id properties of each object within that array. We will use those to get the full information of each monster, instead of just the name

---
+++ DndApp/DndApp/Models/JsonToMonster.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DndApp.Models
+{
+    public class JsonToMonster
+    {
+        // counterpart to array results from JSON (the only part we're interested in)
+        public List<Result> results { get; set; }
+
+        // objects from array results
+        public class Result
+        {
+            [JsonProperty(PropertyName = "index")]
+            public string MonsterId { get; set; }
+            public string Name { get; set; }
+
+
+            public override string ToString()
+            {
+                return $"{this.Name}";
+            }
+        }
+    }
+}
--- DndApp/DndApp/Repositories/MonsterRepository.cs
+++ DndApp/DndApp/Repositories/MonsterRepository.cs
@@ -1,7 +1,10 @@
 ﻿using DndApp.Models;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,10 +32,21 @@ namespace DndApp.Repositories
             {
                 try
                 {
+                    // PROBLEM: the monsters we want are referred to in a url within a object part of a results array.
+
+
                     string json = await client.GetStringAsync(url);
-
-                    List<Monster> monsters = JsonConvert.DeserializeObject<List<Monster>>(json);
-
+                    List<Monster> monsters = new List<Monster>();
+                    List<JsonToMonster.Result> results = new List<JsonToMonster.Result>();
+                    // first we deserialize the jsonstring into an object of the the class we created
+                    // that class has one property which is an array / list of results, within the class we have another class which gets the id and name property of each of the objects within that results array
+                    var data = JsonConvert.DeserializeObject<JsonToMonster>(json);
+                    results = data.results;
+                    
+                    foreach (JsonToMonster.Result result in results)
+                    {
+                        Debug.WriteLine(result);
+                    }
                     return monsters;
                 }
                 catch (Exception ex)
