3e72404bb64a97fdc4dca632fc9ac404cb4e907d
Ian Van Cauwenberg <ian.van.cauwenberg@student.howest.be>
2021-11-25 09:25:25 +0100

    Major layout bugs fixed
    Icons are now centered with labels by replacing stacklayout with grid
    
    Made the entry's row a bit higher to avoid the text being cut off

---
+++ DndApp/DndApp/Entries/StyledSearchBarRenderer.cs
@@ -0,0 +1,22 @@
+﻿using Android.Content;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+//[assembly: ExportRenderer(typeof(searchTab), typeof(StyledSearchBarRenderer))]
+//namespace RestaurantApp.Droid.Renderers
+//{
+//    class StyledSearchBarRenderer : SearchBarRenderer
+//    {
+//        protected override void OnElementChanged(ElementChangedEventArgs<SearchBar> e)
+//        {
+//            var searchView = base.Control as SearchView;
+//            int searchIconId = Context.Resources.GetIdentifier("android:id/search_mag_icon", null, null);
+//            ImageView searchViewIcon = (ImageView)searchView.FindViewById<ImageView>(searchIconId);
+//            searchViewIcon.SetImageDrawable(null);
+//        }
+//    }
+//}
